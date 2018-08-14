757c812f732aead9683d6644bcb0613fb395c213
Mikael Jenkler <mikael@jenkler.com>
2018-08-14 17:16:11 +0200

    Replaced tabs with 2 spaces Updated project dependencies

--- generate.js
+++ generate.js
@@ -5,10 +5,10 @@ const minify = require('gulp-clean-css');
 const sass = require('gulp-sass');
 
 gulp.task('scss', function() {
-	return gulp.src('src/scss/*.scss')
-	.pipe(sass())
-	.pipe(gulp.dest('src/css'))
-	.pipe(minify())
-	.pipe(gulp.dest('src/css.minified'));
+  return gulp.src('src/scss/*.scss')
+  .pipe(sass())
+  .pipe(gulp.dest('src/css'))
+  .pipe(minify())
+  .pipe(gulp.dest('src/css.minified'));
 });
 gulp.start('scss');
