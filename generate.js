98df4b55daec2a0e63d9e7150124856131659765
Mikael Jenkler <mikael@jenkler.com>
2020-02-12 18:29:00 +0100

    Added support for node v12

--- generate.js
+++ generate.js
@@ -5,10 +5,11 @@ const minify = require('gulp-clean-css');
 const sass = require('gulp-sass');
 
 gulp.task('scss', function() {
+  console.log('Creating files');
   return gulp.src('src/scss/*.scss')
   .pipe(sass())
   .pipe(gulp.dest('src/css'))
   .pipe(minify())
   .pipe(gulp.dest('src/css.minified'));
 });
-gulp.start('scss');
+gulp.series('scss')();
