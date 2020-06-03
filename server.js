306eeb239e658e9b9344d8f6754dae999397bd50
Mikael Jenkler <mikael@jenkler.com>
2020-06-03 14:18:18 +0200

    Fixed query string bug

--- server.js
+++ server.js
@@ -3,21 +3,20 @@
 const fs = require('fs');
 const https = require('https');
 const options = {
-  key: fs.readFileSync('localhost.key'),
-  cert: fs.readFileSync('localhost.crt')
+  cert: fs.readFileSync('localhost.crt'),
+  key: fs.readFileSync('localhost.key')
 };
 const path = require('path');
 
 https.createServer(options, function(request, response) {
-  let filePath = './src/css' + request.url;
-  if(request.url == '/') {
-    filePath = './src/css/custom.css';
-  }
+  let url = request.url.split('?')[0];
+  let filePath = './src/css' + url;
+  if(url == '/') filePath = './src/css/custom.css';
   console.log('GET', filePath);
 
-  let extname = String(path.extname(filePath)).toLowerCase();
+  let ext = String(path.extname(filePath)).toLowerCase();
   let mimeTypes = {'.css': 'text/css', '.html': 'text/html', '.js': 'text/javascript'};
-  let contentType = mimeTypes[extname] || 'application/octet-stream';
+  let contentType = mimeTypes[ext] || 'application/octet-stream';
   fs.readFile(filePath, function(error, content) {
     if(error) {
       if(error.code == 'ENOENT') {
